463cda248f3eef2eb45e513e31c1f02658cf7c7e
Octa <ruarteoctavio8@gmail.com>
2022-12-21 13:47:22 -0300

    fix

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import * as express from 'express';
+import * as dotenv from 'dotenv'
+dotenv.config()
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, () => {
+  console.log(`Example app listening on port: ${PORT}`);
 });
