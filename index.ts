1b8d32a16bd234cdfe99032730f5b26bbc33b850
Octa <ruarteoctavio8@gmail.com>
2022-12-21 13:40:24 -0300

    asd

---
+++ index.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+const app = express();
+app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
